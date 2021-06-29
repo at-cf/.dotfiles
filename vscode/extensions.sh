2b3c6789cdb94159092ee92e182bb9c64a3e0c56
cf <64846322+at-cf@users.noreply.github.com>
2021-06-29 09:02:58 -0300

    revert: unused vscode extensions

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -1,18 +1,12 @@
 #! /bin/bash
 
-code --install-extension christian-kohler.path-intellisense
-code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension EditorConfig.EditorConfig
-code --install-extension eg2.vscode-npm-script
 code --install-extension esbenp.prettier-vscode
 code --install-extension johnpapa.vscode-peacock
 code --install-extension vscode-icons-team.vscode-icons
 code --install-extension vscodevim.vim
-code --install-extension idleberg.icon-fonts
-code --install-extension alefragnani.bookmarks
 code --install-extension mikestead.dotenv
-code --install-extension gruntfuggly.todo-tree
 code --install-extension alefragnani.project-manager
 
 
