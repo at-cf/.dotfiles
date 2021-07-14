fb4b338c7d65bd04ec13dbeabb9b5bf3ae8f46c7
cf <64846322+at-cf@users.noreply.github.com>
2021-07-14 10:19:53 -0300

    feat: basic choco packages

---
+++ choco.bat
@@ -0,0 +1,8 @@
+choco install -y --limit-output git --params "/GitOnlyOnPath /NoAutoCrlf /WindowsTerminal /NoShellIntegration /NoGitLfs /SChannel"
+choco install -y --limit-output nodejs.install
+choco install -y --limit-output python
+choco install -y --limit-output notepadplusplus.install
+choco install -y --limit-output vscode
+choco install -y --limit-output 7zip
+choco install -y --limit-output winmerge
+choco install -y --limit-output vim
